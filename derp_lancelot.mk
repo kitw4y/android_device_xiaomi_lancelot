1e6acfc0542aac76f53553126b2fe5d5e37f4e82
kitw4y <91876095+kitw4y@users.noreply.github.com>
2023-01-18 20:53:29 +0700

    Bringup

--- derp_lancelot.mk
+++ derp_lancelot.mk
@@ -12,9 +12,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lancelot
+# Inherit some common DerpFest stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_LEGACY_BOOTANIMATION := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_USES_BLUR := true
+
+PRODUCT_NAME := derp_lancelot
 PRODUCT_DEVICE := lancelot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
