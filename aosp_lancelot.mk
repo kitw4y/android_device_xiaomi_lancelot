22cad98a58cdc3ae566a4ac1a1ac49c87883d718
Asce-XOX <143834552+Asce-XOX@users.noreply.github.com>
2023-09-14 02:01:22 +0530

    Update aosp_lancelot.mk

--- aosp_lancelot.mk
+++ aosp_lancelot.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_lancelot
+PRODUCT_NAME := derp_lancelot
 PRODUCT_DEVICE := lancelot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
